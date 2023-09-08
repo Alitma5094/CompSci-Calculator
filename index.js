e26874b732967bc5692311930debb18bf65e5352
Andrew Litman <litmanandrew@gmail.com>
2023-09-08 09:38:17 -0400

    Clean up code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function calculateDecimal(event) {
-    if (!otherCheckbox.checked) {
-        const binarySwitches = document.querySelectorAll('#simpleList');
+function calculateDecimal() {
+    if (!hideDecimalCheckbox.checked) {
+        const binarySwitches = Array.prototype.slice.call(document.querySelectorAll('.binaryList')).reverse();
 
         let decimalValue = 0;
 
@@ -14,53 +14,54 @@ function calculateDecimal(event) {
     }
 }
 
-// Create binary switches for decimal numbers 1, 2, 4, 8, 16, 32, 64, etc.
-// const binaryContainer = document.getElementById('binary-container');
-const binaryHeader = document.getElementById('binary-header');
-const binaryBody = document.getElementById('binary-body');
-const maxDecimalValue = 256; // You can adjust this as needed
+const hideDecimalCheckbox = document.querySelector("#decimalCheckbox");
+hideDecimalCheckbox.addEventListener("change", () => {
+    if (hideDecimalCheckbox.checked) {
+        document.getElementById('decimal-value').textContent = "???";
+    } else {
+        calculateDecimal()
+    }
+});
 
+const binaryHeader = document.getElementById('binaryHeader');
+const binaryPower = document.getElementById('binaryPower');
+const binaryBody = document.getElementById('binaryBody');
+const maxDecimalValue = 128;
+
+var power = 0
 for (let decimal = 1; decimal <= maxDecimalValue; decimal *= 2) {
     const newHeader = document.createElement("th")
     newHeader.textContent = decimal;
     binaryHeader.appendChild(newHeader);
 
-    const switchInput = document.createElement('input');
-    switchInput.type = 'checkbox';
-    switchInput.classList.add("binary-switch")
-    switchInput.addEventListener('change', calculateDecimal);
+    const newPower = document.createElement("td")
+    newPower.innerHTML = `2<sup>${power}</sup>`
+    power++;
+    binaryPower.appendChild(newPower);
 
     const newBodyItem = document.createElement("td")
     Sortable.create(newBodyItem, {
         swap: true,
-        swapClass: "text-primary",
+        swapClass: "text-bg-success",
         animation: 150,
         onSort: calculateDecimal,
     });
-    newBodyItem.id = "simpleList"
+    newBodyItem.classList.add("binaryList")
     newBodyItem.innerHTML = `
 <div class="card text-bg-secondary mb-3 zero-card" style="max-width: 18rem;">
-<div class="card-body">
+<div class="card-body mx-auto">
 <h5 class="card-title">0</h5>
 </div>
 </div>
-<div class="card text-bg-success mb-3 one-card" style="max-width: 18rem;">
-<div class="card-body">
+<div class="card text-bg-info mb-3 one-card" style="max-width: 18rem;">
+<div class="card-body mx-auto">
 <h5 class="card-title">1</h5>
 </div>
 </div>
 `;
     binaryBody.appendChild(newBodyItem)
 }
-
-
-
-
-const otherCheckbox = document.querySelector("#scales");
-otherCheckbox.addEventListener("change", () => {
-    if (otherCheckbox.checked) {
-        document.getElementById('decimal-value').textContent = "???";
-    } else {
-        calculateDecimal()
-    }
-});
+for (var i = 1; i < binaryHeader.childNodes.length; i++){
+    binaryHeader.insertBefore(binaryHeader.childNodes[i], binaryHeader.firstChild);
+    binaryPower.insertBefore(binaryPower.childNodes[i], binaryPower.firstChild);
+}
